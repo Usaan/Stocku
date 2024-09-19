7c47cffa9e944ba06bdf7dc417e86b275165e2e6
Usaan <gabrielrebecacr@gmail.com>
2024-09-19 11:57:09 -0300

    Modularização do listamento de produtos [LIST1]

--- main.c
+++ main.c
@@ -5,36 +5,7 @@
 #include <locale.h>
 #include "verifystorage.h"
 #include "findprodmethods.h"
-
-int selected_product, selected_qty, ID;
-
-void list_products(produto *arrayProdutos, int qtd) {
-    printf("---------------------------------------------------------------\n");
-    printf("%-10s%-19s%-10s%-10s\n", "ID", "Nome", "Qtd", "Preço");
-    for (int i = 0; i < qtd; i++) {
-        printf("%-10d%-19s%-10dR$%-10.2f\n",
-        arrayProdutos[i].id,
-        arrayProdutos[i].name,
-        arrayProdutos[i].quantity,
-        arrayProdutos[i].price);
-    }
-    printf("---------------------------------------------------------------\n");
-    fflush(stdin);
-    getchar();
-    fflush(stdin);
-    system("cls");
-}
-void list_products_simple(produto *arrayProdutos, int qtd) {
-    printf("-----------------------------------------\n");
-    printf("%-10s%-19s%-10s\n", "ID", "Nome", "Qtd");
-    for (int i = 0; i < qtd; i++) {
-        printf("%-10d%-19s%-10d\n",
-        arrayProdutos[i].id,
-        arrayProdutos[i].name,
-        arrayProdutos[i].quantity);
-    }
-    printf("-----------------------------------------\n");
-}
+#include "listingprods.h"
 
 void menu(produto *arrayProdutos, int qtd) {
 
