b7628b9b524e6cfff12385de5c5299eab38206e2
Usaan <gabrielrebecacr@gmail.com>
2024-09-19 23:16:44 -0300

    Comentários para as verificações de estoque

--- src/verifystorage.c
+++ src/verifystorage.c
@@ -2,6 +2,15 @@
 
 int qty = 0;
 
+/**
+ * @brief Função que retorna a quantidade de itens no database.
+ *
+ * @details A função verifica se o diretório "database" existe, caso não exista, o mesmo é criado.
+ * Abre o arquivo "database/storage.txt" em modo de leitura e conta quantas linhas ele tem, armazenando o valor na variável 
+ * global "qty".
+ *
+ * @return Retorna o valor de "qty", que é a quantidade de itens no database.
+ */
 int qtyItems(){
     mkdir("database");
     FILE *ptrStorage = fopen("database/storage.txt", "r");
@@ -15,6 +24,16 @@ int qtyItems(){
     fclose(ptrStorage);
     return qty;
 }
+/**
+ * @brief Função que importa os dados do database para um array de structs.
+ *
+ * @details A função abre o arquivo "database/storage.txt" em modo de leitura e aloca memória com o malloc
+ * para um array de structs com o tamanho igual a quantidade de itens detectada.
+ * Em seguida, lê as linhas do arquivo e armazena as informações em cada campo do array de structs.
+ * Por fim, fecha o arquivo e retorna o array de structs.
+ *
+ * @return Um ponteiro para o array de structs >produto< com os dados do database.
+ */
 produto *import_data() {
     FILE *ptrStorage = fopen("database/storage.txt", "r");
     produto *arrayProdutos = malloc(sizeof(produto) * qty);
