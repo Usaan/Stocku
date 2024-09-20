848720f4c019c11b22ec3469a665568eeb750d37
Usaan <gabrielrebecacr@gmail.com>
2024-09-19 23:16:20 -0300

    Comentários para a função main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include "menu.h"
 
+/**
+ * @brief Função o principal do programa.
+ *
+ * @param argc Numero mero de argumentos passados na linha de comando.
+ * @param argv Vetor de strings com os argumentos passados na linha de comando.
+ *
+ * @return O valor de retorno do programa para o SO.
+ *
+ * @details A função seta o console de entrada e saida para o UTF-8, pega a quqantidade de itens no database e importa os 
+ * dados dele para um array de structs. Em seguida, chama a função menu e finaliza o programa liberando a memória alocada.
+ */
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     SetConsoleOutputCP(CP_UTF8);
